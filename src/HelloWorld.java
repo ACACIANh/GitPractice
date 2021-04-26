f34b8721a69382927616b8d04558e40ff41fb516
STU <STU@DESKTOP-UG11JUO>
2021-04-26 10:18:31 +0900

    add Function1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 
 public class HelloWorld {
+	static void Function1() {
+		System.out.println("Function1");
+			
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
