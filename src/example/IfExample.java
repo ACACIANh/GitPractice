f5aaed411c9003ecf838654823ab26421928672f
DESKTOP-UG11JUO\STU <hahaha>
2021-04-27 18:37:10 +0900

    task compelte

--- src/example/IfExample.java
+++ src/example/IfExample.java
@@ -6,10 +6,21 @@ public class IfExample {
 		//단, 변수에 저장된 문자값이 소문자인 경우 대문자로 변환하여 출력하세요.
 		char mun='x';
 		
+		if(mun >= 97) {
+			mun -= 32;
+		}
+		
 		System.out.println("mun = "+mun);
 		System.out.println("============================================================");
 		//변수에 저장된 정수값이 4의 배수인지를 구분하여 출력하세요.
 		int num=345644;
+		
+		if(num % 4 == 0) {
+			System.out.println("4의 배수입니다.");
+		}
+		else {
+			System.out.println("4의 배수가 아닙니다.");
+		}
 		
 		
 		System.out.println("============================================================");
@@ -18,6 +29,19 @@ public class IfExample {
 		// => 위 조건을 만족하는 년도 중 100으로 나누어 나머지가 0인 경우 평년
 		// => 위 조건을 만족하는 년도 중 400으로 나누어 나머지가 0인 경우 윤년
 		int year=2021;
+		
+		if(year % 400 == 0) {
+			System.out.println("윤년입니다.");
+		}
+		else if(year % 100 == 0) {
+			System.out.println("평년입니다.");
+		}
+		else if(year % 4 == 0) {
+			System.out.println("윤년입니다.");
+		}
+		else {
+			System.out.println("평년입니다.");
+		}
 		
 		
 		System.out.println("============================================================");
@@ -28,7 +52,43 @@ public class IfExample {
 		// => 평균을 이용한 학점 계산 : 100~90:A, 89~80:B, 79~70:C, 69~60:D, 59~0:F
 		// => 평균은 소숫점 두자리까지만 출력하고 나머지는 절삭 처리 하세요.
 
+		String name = "홍길동";
+		int korean = 89;
+		int english = 93;
+		int math = 95;
+		int sum = korean + english+ math;
+		double average = sum / 3.0;
+		char grade = '\0'; 
 		
+		if( (korean < 0 || 100 < korean) || 
+			(english < 0 || 100 < english) || 
+			(math < 0 || 100 < math) ) {
+			System.out.println("점수가 잘못 되었음");
+			System.exit(0);
+		}
+		switch((int)average / 10) {
+		case 10:
+		case 9:
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+			break;
+		case 7:
+			grade = 'C';
+			break;
+		case 6:
+			grade = 'D';
+			break;
+		default:
+			grade = 'F';		
+			break;
+		}		
+		
+		System.out.println("이름 : " + name);
+		System.out.println("총점 : " + sum);
+		System.out.println("평균 : " + (int)(average * 100) / 100.0);
+		System.out.println("학점 : " + grade);
 		System.out.println("============================================================");
 	}
 }
--- src/example/OperatorExample.java
+++ src/example/OperatorExample.java
@@ -3,14 +3,37 @@ package example;
 public class OperatorExample {
 	public static void main(String[] args) {
 		//245678초를 일시분초 형식으로 변환하여 출력하세요.
+		int time = 245678;
+		int hour;
+		int miniute;
+		int second;
 		
-		
+		second = time % 60;
+		time /= 60;
+		miniute = time % 60;
+		time /= 60;
+		hour = time % 60;
+		System.out.println(hour + " 시 " + miniute + " 분 " + second + " 초 ");
 		System.out.println("===============================================");
+		
 		//한대의 가격이 1억 5천원만인 비행기를 20대 구매할 경우 지불해야될 금액을 계산하여 
 		//출력하세요. 
 		//단, 15대 이상 구매할 경우 1대당 25%의 할인율을 적용하여 계산하세요. 
+		double price = 150_000_000;
+		int count = 20;
+		double cost = price * count;
 
+		if(count >= 15) {
+			int saleCount = count - 15;
+			cost -= price * saleCount * 0.25;
+		}
 		
+		//3항 연산자를 이용할 경우
+		System.out.println( 
+				(count < 15 ? price * count : price * 15 + price * (count - 15) * 0.75)
+						+ " 원 입니다.");
+		
+		System.out.println(cost + " 원 입니다.");
 		System.out.println("===============================================");
 	}
 }
